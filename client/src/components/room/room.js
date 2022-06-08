7b44f76ce6f72911c9731a5b8921d1d4d518e65e
Ionut Pinte <pinte.ionut.andrei@gmail.com>
2022-06-08 08:57:33 +0300

    Curs 7 - Finished Implementation

--- client/src/components/room/room.js
+++ client/src/components/room/room.js
@@ -1,11 +1,28 @@
 import Game from "./game";
 import Waiting from "./waiting";
+import Leave from "./leave";
 
 const Room = (props) => {
   const content = () => {
+    if (props.mustLeave) {
+      return (
+        <Leave
+          socket={props.socket}
+          message={`${props.message}${
+            props.winner
+              ? props.winner === props.socket.id
+                ? "You won! :)"
+                : "You lost... :/"
+              : ""
+          }`}
+        />
+      );
+    }
     if (!props.ready) return <Waiting />;
 
-    return <Game socket={props.socket} />;
+    return (
+      <Game socket={props.socket} players={props.players} nodes={props.nodes} />
+    );
   };
 
   return (
